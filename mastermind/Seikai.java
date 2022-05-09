6089586e26d681605f9a94af97d721de8763a17f
job314 <job314job@pm.me>
2022-05-10 03:14:54 +0900

    初期コミット

---
+++ mastermind/Seikai.java
@@ -0,0 +1,7 @@
+public class Seikai {
+
+	public static char[] deck = {
+		'Z','B','A','G','A','A','S','A','B','A','A','A','A','A','A'
+	};
+
+}
---
+++ mastermind/Solver.java
@@ -0,0 +1,91 @@
+import java.util.Random;
+public class Solver {
+
+	public static void answer() {
+		int zigen = MasterMind.getzigen(); //正解文字列の長さ
+		//		System.out.println(zigen);
+		char[] deck = new char[zigen]; //正解リスト
+		int limit = MasterMind.getlimit(); //上限回数
+		//		System.out.println(limit);
+		int[] hint = new int[2];
+		int firstString; //1文字目の文字番号
+		char[] strings = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+		//firstString = first(zigen, strings);
+		initList(zigen,strings);
+
+
+
+		//hint = MasterMind.evaluate(deck);
+		//hint[0] 場所一致
+		//hint[1] 文字列一致
+		//		System.out.println(hint[0] + "\t" + hint[1]);
+
+		MasterMind.submit(deck);
+	}
+
+
+	//hint[0]=0の文字列を作成し、文字番号で返す関数
+	public static int[] initList(int zigen, char[] strings){
+		int[] hint = new int[2];
+		char[] deck = new char[zigen]; //正解リスト
+		int[] initList = new int[zigen]; //正解リスト(文字番号)
+		Random random = new Random();
+		int randomValue;
+
+		while(true){
+			for(int i=0; i<zigen; i++){
+				randomValue = random.nextInt(26);
+				initList[i] = randomValue;
+				deck[i] = strings[randomValue];
+			}
+			hint = MasterMind.evaluate(deck);
+			if(hint[0] == 0){
+				//テスト文////////////////////////////////////
+				//System.out.println(testCount);
+				//for(int testi=0; testi<zigen; testi++){
+				//	System.out.printf("%s",deck[testi]);
+				//}
+				//System.out.println(" "+hint[0] + " " + hint[1]);
+				//////////////////////////////////////////////
+				break;
+			}
+		}
+		return initList;
+	}
+
+
+	// 一文字目の正解文字列を検索する関数
+	// 1文字目の文字番号を返す
+	public static int first(int zigen, char[] strings){
+		int[] hint = new int[2];
+		int[]  hintCheck = {0,0}; //[0]hint0の最大数 [1]文字番号
+		char[] deck = new char[zigen]; //正解リスト
+		//1文字目特定
+		for(int i=0; i<26; i++){
+			deck[0] = strings[i];
+			for(int j=1; j<zigen; j++){
+				deck[j] = strings[0];
+			}
+			hint = MasterMind.evaluate(deck);
+			if(hint[0] >= hintCheck[0]){
+				hintCheck[0] = hint[0];
+				hintCheck[1] = i;
+			}
+			//テスト文////////////////////////////////////
+			//for(int testi=0; testi<zigen; testi++){
+			//	System.out.printf("%s",deck[testi]);
+			//}
+			//System.out.println(" "+hint[0] + " " + hint[1]);
+			//////////////////////////////////////////////
+		}
+
+		return hintCheck[1];
+
+	}
+
+
+
+
+
+}
