afefc66da811e5edfe7e2c3e11196fb8a709c8e2
job314 <job314job@pm.me>
2022-06-03 16:14:03 +0900

    ファイル追加

---
+++ pattern/files-20220603/pattern/Solver.java
@@ -0,0 +1,13 @@
+public class Solver {
+
+	public static String[] answer() {
+		String[] kaitou = new String[Data.numdata];
+
+		for(int i=0; i<Data.numdata; i++){
+			kaitou[i]="C1";
+		}
+
+		return kaitou;
+	}
+
+}
